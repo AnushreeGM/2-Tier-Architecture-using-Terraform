7c054c1642f8c77fca2c35125a3945d79d827555
EC2 Default User <ec2-user@ip-172-31-9-27.eu-north-1.compute.internal>
2025-05-11 08:42:57 +0000

    Rectified error

--- root/main.tf
+++ root/main.tf
@@ -68,20 +68,20 @@ module "rds" {
 
 
 # create cloudfront distribution 
-module "cloudfront" {
-  source = "../modules/cloudfront"
-  certificate_domain_name = var.certificate_domain_name
-  alb_domain_name = module.alb.alb_dns_name
-  additional_domain_name = var.additional_domain_name
-  project_name = module.vpc.project_name
-}
+# module "cloudfront" {
+#   source = "../modules/cloudfront"
+#   certificate_domain_name = var.certificate_domain_name
+#   alb_domain_name = module.alb.alb_dns_name
+#   additional_domain_name = var.additional_domain_name
+#   project_name = module.vpc.project_name
+# }
 
 
 # Add record in route 53 hosted zone
 
-module "route53" {
-  source = "../modules/route53"
-  cloudfront_domain_name = module.cloudfront.cloudfront_domain_name
-  cloudfront_hosted_zone_id = module.cloudfront.cloudfront_hosted_zone_id
+# module "route53" {
+#   source = "../modules/route53"
+#   cloudfront_domain_name = module.cloudfront.cloudfront_domain_name
+#   cloudfront_hosted_zone_id = module.cloudfront.cloudfront_hosted_zone_id
 
-}
+# }
